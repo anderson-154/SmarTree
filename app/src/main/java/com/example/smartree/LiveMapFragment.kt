c60a505ca73ee33390bde1ffebb2b260f4172ca4
Benjamin Silva <benjamin.silva@insight4.tech>
2023-02-15 23:26:22 -0500

    feature(maps): Decorate view card palm

--- app/src/main/java/com/example/smartree/LiveMapFragment.kt
+++ app/src/main/java/com/example/smartree/LiveMapFragment.kt
@@ -1,6 +1,7 @@
 package com.example.smartree
 
 import android.content.Intent
+import android.content.res.ColorStateList
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -12,6 +13,7 @@ import com.example.smartree.model.Palm
 import com.example.smartree.model.Sensor
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+
 
 class LiveMapFragment : Fragment (), MapsFragment.OnClickMarkerListener {
 
@@ -66,6 +68,16 @@ class LiveMapFragment : Fragment (), MapsFragment.OnClickMarkerListener {
             db.collection("sensors").document(palm.sensorID).get().addOnSuccessListener {
                 val sensor = it.toObject(Sensor::class.java)!!
                 binding.statePinTV.text = sensor.state
+                if(sensor.state=="Sano"){
+                    binding.statePinTV.setTextColor(resources.getColor(R.color.dark_green))
+                    binding.iconPin.setImageResource(R.drawable.health)
+                }else if(sensor.state=="Sospechoso"){
+                    binding.statePinTV.setTextColor(resources.getColor(R.color.dark_orange))
+                    binding.iconPin.setImageResource(R.drawable.warning)
+                }else{
+                    binding.statePinTV.setTextColor(resources.getColor(R.color.red))
+                    binding.iconPin.setImageResource(R.drawable.bug)
+                }
             }
 
             //Set names
